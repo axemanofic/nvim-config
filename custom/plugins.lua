638ecfa9d92bdea435bf625fb3dc23347d853705
Roman Sotnikov <axeman.ofic@gmail.com>
2023-08-28 20:19:22 +0300

    feat: add new dashboard

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -2,9 +2,6 @@ local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
-
-	-- Override plugin definition options
-
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -19,26 +16,20 @@ local plugins = {
 		config = function()
 			require("plugins.configs.lspconfig")
 			require("custom.configs.lspconfig")
-		end, -- Override to setup mason-lspconfig
+		end,
 	},
-
-	-- override plugin configs
 	{
 		"williamboman/mason.nvim",
 		opts = overrides.mason,
 	},
-
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = overrides.treesitter,
 	},
-
 	{
 		"nvim-tree/nvim-tree.lua",
 		opts = overrides.nvimtree,
 	},
-
-	-- Install a plugin
 	{
 		"max397574/better-escape.nvim",
 		event = "InsertEnter",
@@ -46,20 +37,14 @@ local plugins = {
 			require("better_escape").setup()
 		end,
 	},
-
-	-- To make a plugin not be loaded
-	-- {
-	--   "NvChad/nvim-colorizer.lua",
-	--   enabled = false
-	-- },
-
-	-- All NvChad plugins are lazy-loaded by default
-	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-	-- {
-	--   "mg979/vim-visual-multi",
-	--   lazy = false,
-	-- }
+	{
+		"glepnir/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+      require("custom.configs.dashboard")
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
 }
 
 return plugins
