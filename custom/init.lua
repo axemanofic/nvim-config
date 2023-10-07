9996a6474b34a267a12a010d5e9348ec94efe079
Roman Sotnikov <axeman.ofic@gmail.com>
2023-10-07 18:21:19 +0300

    fix: update defaults options

--- custom/init.lua
+++ custom/init.lua
@@ -1,8 +1,12 @@
 vim.opt.relativenumber = true
+vim.opt.hidden = true
+vim.opt.backup = false
+vim.opt.swapfile = true
+vim.opt.wrap = false
 
 -- See: https://github.com/neovim/nvim-lspconfig/tree/54eb2a070a4f389b1be0f98070f81d23e2b1a715#suggested-configuration
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
